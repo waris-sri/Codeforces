c5ca9e443e87ac94b051b030376897d949e87760
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2024-08-08 14:00:04 +0700

    +1C

---
+++ C Implementation/word_streamlined.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    char t[100];
+    scanf("%s", t);
+    int lo = 0;
+    int up = 0;
+    
+    for (int i = 0; i < strlen(t); i++)
+    {
+        if (t[i] >= 'A' && t[i] <= 'Z')
+        {
+            up++;
+        }
+        if (t[i] >= 'a' && t[i] <= 'z')
+        {
+            lo++;
+        }
+    }
+
+    if (lo >= up)
+    {
+        for (int i = 0; i < strlen(t); i++)
+        {
+            t[i] = tolower(t[i]);
+        }        
+    }
+    else
+    {
+        for (int i = 0; i < strlen(t); i++)
+        {
+            t[i] = toupper(t[i]);
+        }
+    }
+    printf("%s\n", &t);
+    return 0;
+}
