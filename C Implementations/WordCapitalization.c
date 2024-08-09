b3a1eef0db5dfae4771acdc2f687369c8d20cf42
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2024-08-09 21:38:17 +0700

    +1C

--- C Implementations/WordCapitalization.c
+++ C Implementations/WordCapitalization.c
@@ -2,9 +2,9 @@
 
 int main() {
   char t[1000];
-  scanf("%*s", t);
+  scanf("%[^\n]", t); // scan until \n character (regex)
   if (t[0] >= 'a' && t[0] <= 'z') {
-    t[0] -= 32;
+    t[0] -= 32; // turn to uppercase (95 'A' - 67 'a')
   }
-  printf("%s", t);
+  printf("%s\n", t);
 }
