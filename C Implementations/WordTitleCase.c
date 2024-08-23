31af02cc8d5ba1a991a86eac1fe6f0e8361d2142
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-08-24 01:22:19 +0700

    Updated

--- C Implementations/WordTitleCase.c
+++ C Implementations/WordTitleCase.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+int main() {
   char t[1000];
   char str = scanf("%[^\n]", t); // scan until \n character (regex)
   const char delim[2] = " ";     // use " " as a delimiter, size 2 for '\n'
   char *tok;                     // prepare for tokenizer
   tok = strtok(t, delim);
-  while (tok != 0)
-  {
+  while (tok != 0) {
     // capitalize each token's first character'
-    if (tok[0] >= 'a' && tok[0] <= 'z')
-    {
+    if (tok[0] >= 'a' && tok[0] <= 'z') {
       tok[0] -= 32; // turn to uppercase (95 'A' - 67 'a')
     }
     printf("%s ", tok);     // print out each token followed by a space
