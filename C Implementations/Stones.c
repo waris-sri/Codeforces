dda6c44bd54367b0b6e0b87d5fead05f2786a0f8
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-09-30 19:32:33 +0700

    +1

--- C Implementations/Stones.c
+++ C Implementations/Stones.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
   int n;
   scanf("%d", &n);
   char stones[n];
   scanf("%s", stones);
   char curr = stones[0];
   int cnt = 0;
-  for (int i = 1; i < n; i++)
-  {
-    if (stones[i] != curr)
-    {
+  for (int i = 1; i < n; i++) {
+    if (stones[i] != curr) {
       curr = stones[i];
-    }
-    else
-    {
+    } else {
       cnt += 1;
     }
   }
